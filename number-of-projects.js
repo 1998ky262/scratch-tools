075eac145fc682b420407b09df7b2dee263343a8
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-10-30 00:12:13 +0900

    Delete number-of-projects.js

--- number-of-projects.js
+++
@@ -1,8 +0,0 @@
-const url = location.href;
-const studioId = url.replace(/[^0-9]/g, '');
-async function insaneGuy(){
-const a = await fetch(`https://api.scratch.mit.edu/studios/${username}/`);
-const b = await a.json();
-  console.log(b);
-}
-insaneGuy();
