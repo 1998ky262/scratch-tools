4db028c1efa7eace5e6f3f914eff4adbec9703e1
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-10-30 00:10:21 +0900

    Update number-of-projects.js

--- number-of-projects.js
+++ number-of-projects.js
@@ -1,7 +1,7 @@
 const url = location.href;
 const studioId = url.replace(/[^0-9]/g, '');
 async function insaneGuy(){
-const a = await fetch(`https://api.scratch.mit.edu/users/${username}/messages/count`);
+const a = await fetch(`https://api.scratch.mit.edu/studios/${username}/`);
 const b = await a.json();
   console.log(b);
 }
