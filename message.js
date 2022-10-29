b2121be076e5cee24538cd2d54d1debbd5275350
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-10-29 23:59:32 +0900

    Update message.js

--- message.js
+++ message.js
@@ -2,6 +2,6 @@ let username=prompt("通知数を知りたいユーザー名を入力");
 async function insaneGuy(){
 const a = await fetch(`https://api.scratch.mit.edu/users/${username}/messages/count`);
 const b = await a.json();
-console.log(b);
 }
 insaneGuy();
+alert(username+"の通知数は"+b+"です");
