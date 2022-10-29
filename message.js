51e2579b26b271428b74b7ed7a8653c56760b7c8
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-10-29 23:58:28 +0900

    Update message.js

--- message.js
+++ message.js
@@ -1,5 +1,6 @@
+let username=prompt("通知数を知りたいユーザー名を入力");
 async function insaneGuy(){
-const a = await fetch("https://api.scratch.mit.edu/users/nekopyon/messages/count");
+const a = await fetch(`https://api.scratch.mit.edu/users/${username}/messages/count`);
 const b = await a.json();
 console.log(b);
 }
