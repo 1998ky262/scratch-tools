2055e7dab980384a1b857ca198a473a7f4c786db
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-10-30 21:09:13 +0900

    Delete Add-studio.js

--- Add-studio.js
+++
@@ -1,6 +0,0 @@
-const url = location.href;
-const projectid = url.replace(/[^0-9]/g, '');
-function addstudio(studioid){
-$.ajax({type:"POST",url:`https://api.scratch.mit.edu/studios/${studioid}/project/${projectid}`});
-}
-addstudio("56");
