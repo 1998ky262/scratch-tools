33968f6b6d891d9d75ae871b4271b34841c9f3be
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-10-30 15:59:46 +0900

    Update Add-studio.js

--- Add-studio.js
+++ Add-studio.js
@@ -1,6 +1,6 @@
 const url = location.href;
 const PROJECTID = url.replace(/[^0-9]/g, '');
 function ADDSTUDIO(STUDIOID){
-$.ajax({type:"POST",url:`https://api.scratch.mit.edu/studios/${STUDIOID}/project/${PROJECTID}'});
+$.ajax({type:"POST",url:`https://api.scratch.mit.edu/studios/${STUDIOID}/project/${PROJECTID}`});
 }
 ADDSTUDIO("56");
