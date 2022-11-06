c0000026c0211cb62410aff75a30a0ae84b8feb2
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-11-06 16:01:02 +0900

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -1,2 +1,12 @@
 fetch("https://scratch.mit.edu/session/",
 {"headers":{"x-requested-with": "XMLHttpRequest"}}).then(response=>response.text()).then(data=>console.log(data));
+async funtion addstudio(STUDIOID){
+await fetch(`https://api.scratch.mit.edu/studios/${STUDIOID}/project/${PROJECTID}`
+ {method:"POST" ,
+    headers:{
+            "x-token":data}
+              );
+}
+const PROJECTID=prompt("プロジェクトのIDを入力");
+addstudio(56);
+ 
