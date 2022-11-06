d6dbce4322cbc55bf8fa5c2669e9d85061d7917a
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-11-06 19:16:02 +0900

    Update TrendSearch.js

--- TrendSearch.js
+++ TrendSearch.js
@@ -1,1 +1,12 @@
-await fetch(`https://api.scratch.mit.edu/explore/projects?limit=1&offset=1&language=ja&mode=&q=trending`)
+funtion trendsearch(th){
+const deta =await fetch(`https://api.scratch.mit.edu/explore/projects?limit=1&offset=${th}&language=ja&mode=trending&q=*`);
+const detatext=await deta.json();
+ console.log(detatext);
+}
+let a=0;
+while(a==1000){
+  trendsearch(a);
+  a++;
+}
+
+
