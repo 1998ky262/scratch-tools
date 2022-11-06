6020ad2319c5b7629ffb9f1e74508fee363178c0
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-11-06 18:41:50 +0900

    Update TrendSearch.js

--- TrendSearch.js
+++ TrendSearch.js
@@ -1,1 +1,1 @@
-await fetch(`https://api.scratch.mit.edu/explore/projects?limit=1&offset=1&language=ja&mode=&q=`)
+await fetch(`https://api.scratch.mit.edu/explore/projects?limit=1&offset=1&language=ja&mode=&q=trending`)
