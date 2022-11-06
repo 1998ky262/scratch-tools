41080905e3829b32487d698be63996ee172ff1e1
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-11-06 19:22:25 +0900

    Update TrendSearch.js

--- TrendSearch.js
+++ TrendSearch.js
@@ -1,7 +1,7 @@
 async function trendsearch(th){
 const deta =await fetch(`https://api.scratch.mit.edu/explore/projects?limit=1&offset=${th}&language=ja&mode=trending&q=*`);
 const detatext=await deta.json();
- console.log(detatext);
+ console.log(detatext["title"]);
 }
 let a=0;
 while(a<1001){
