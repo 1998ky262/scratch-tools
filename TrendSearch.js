57ba29d073702f06e0cdf3fff565658059c9aa41
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-11-07 21:23:07 +0900

    Update TrendSearch.js

--- TrendSearch.js
+++ TrendSearch.js
@@ -1,13 +1,13 @@
 let username=prompt("ユーザー名を入力");
-username=username.toUpperCase();
+let USERNAME=username.toUpperCase();
 let detatext;
 async function trendsearch(){
     let a=0;
     while(a<10001){
         let deta = await fetch(`https://api.scratch.mit.edu/explore/projects?limit=1&offset=${a}&language=ja&mode=trending&q=*`);
         detatext = await deta.json();
-        detatext=detatext[0]["author"]["username"].toUpperCase();
-        if(detatext == username){
+        let DETATEXT=detatext[0]["author"]["username"].toUpperCase();
+        if(detatext == USERNAME){
             console.log(username+"さんの作品「"+detatext[0]["title"]+"」が傾向"+a+"位に載っています");
         }
         a++;
