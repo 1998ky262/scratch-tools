cb3691ed7d5f978e180d60c1ce6f7d0e7bae869c
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-11-07 19:31:34 +0900

    Update TrendSearch.js

--- TrendSearch.js
+++ TrendSearch.js
@@ -1,3 +1,4 @@
+const username=prompt("ユーザー名を入力");
 async function trendsearch(th){
 const deta =await fetch(`https://api.scratch.mit.edu/explore/projects?limit=1&offset=${th}&language=ja&mode=trending&q=*`);
 let detatext=await deta.json();
