226a33e7da50518bd6a503a828d8cd5fc1bfde2e
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-11-13 17:52:10 +0900

    Update NoAdsYoutube.js

--- NoAdsYoutube.js
+++ NoAdsYoutube.js
@@ -1,7 +1,11 @@
 const a = getParam("v");
+// youtubeの動画idを取得
 const move =document.querySelector('html');
 move.remove();
-
+// htmlを消します()
+let wannahtml = document.createElement('html');
+// html要素を作成
+wannahtml.textContent = `<body><iframe width="560" height="315" src="https://www.youtube.com/embed/${a}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></body>`;
 function getParam(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
